4765af6cf9187060ca1addf6ec201a6aad2f4c24
garudaramakrishna <ramakrishna_garuda@epam.com>
2017-06-05 14:03:34 +0530

    Homwork updated with Directives and Routing

---
+++ home-work-angular/app-cat/partials/login.controller.js
@@ -0,0 +1,18 @@
+﻿(function (module) {
+    "use strict";
+    function LoginController(catService,$cookies, $log) {
+        var vm = this;
+        vm.login = login;
+
+        function login(user) {
+            userdb.push(user);
+            localStorage.set(KEY, userdb);
+            $state.go('login');
+            toastr.info('You have successfully created a new account and have been signed-up');
+        }
+    }
+
+    LoginController.$inject = ["voteCatService", "$cookies", "$log"];
+    module.controller("LoginController", LoginController);
+
+})(angular.module("catapp"));
---
+++ home-work-angular/app-cat/partials/signup.controller.js
@@ -0,0 +1,21 @@
+﻿(function (module) {
+    "use strict";
+    function SignUpController(localStorageService, $cookies, $log) {
+        var vm = this, userdb, KEY = 'users';
+        vm.signup = signup;
+
+        userdb = localStorageService.get(KEY);
+        userdb = !userdb ? localStorageService.set(KEY, []) : userdb;
+
+        function signup(user) {
+            userdb.push(user);
+            localStorageService.set(KEY, userdb);
+            $state.go('login');
+            //toastr.info('You have successfully created a new account and have been signed-up');
+        }
+    }
+
+    SignUpController.$inject = ["localStorageService", "$cookies", "$log"];
+    module.controller("SignUpController", SignUpController);
+
+})(angular.module("catapp"));
